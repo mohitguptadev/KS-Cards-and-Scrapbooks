009ac9faa8414b6bbf4a2738338dc955a2118501
Mohit Gupta <mohitkumargupta.com@gmail.com>
2026-09-04 13:00:11 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
-import { db } from "./firebase-config.js";
-
 const menuToggle = document.getElementById("menuToggle");
 const navLinks = document.getElementById("navLinks");
 
 if (menuToggle && navLinks) {
-
     menuToggle.addEventListener("click", () => {
         navLinks.classList.toggle("active");
     });
-
 }
